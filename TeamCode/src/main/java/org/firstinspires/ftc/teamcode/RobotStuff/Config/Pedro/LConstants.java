7669969b90890d2759e2a1a4ca8f12856268c19a
Nucleus (Anders) <aksandvick@hotmail.com>
2025-04-18 10:15:39 -0400

    Updated Config (Pinpoint Locations)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStuff/Config/Pedro/LConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStuff/Config/Pedro/LConstants.java
@@ -7,8 +7,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class LConstants {
     static {
-        PinpointConstants.forwardY = -3.3070;
-        PinpointConstants.strafeX = -6.6141;
+        PinpointConstants.forwardY = 0.984252;
+        PinpointConstants.strafeX = -4.92126;
         PinpointConstants.distanceUnit = DistanceUnit.INCH;
         PinpointConstants.hardwareMapName = "sensor";
         PinpointConstants.useYawScalar = false;
