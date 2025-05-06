49ae3cb5d07099ba13a995bb3cb2b6679f3c7c9d
yahaha5788 <j.thomps1310@gmail.com>
2025-05-06 08:37:35 -0400

    directory for demobot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoBotOpModes/LifterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoBotOpModes/LifterTest.java
@@ -1,25 +1,20 @@
-package org.firstinspires.ftc.teamcode.TeleOpModes;
+package org.firstinspires.ftc.teamcode.DemoBotOpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import com.rowanmcalpin.nextftc.ftc.NextFTCOpMode;
 
 import org.firstinspires.ftc.teamcode.RobotStuff.Config.Subconfigs.DemoRobotConfig;
-import org.firstinspires.ftc.teamcode.RobotStuff.Config.Subconfigs.RobotConfig;
-import org.firstinspires.ftc.teamcode.RobotStuff.IndividualComponents.DemoBot.DemoBotDrive;
-import org.firstinspires.ftc.teamcode.RobotStuff.IndividualComponents.DemoBot.DemoBotLauncher;
-import org.firstinspires.ftc.teamcode.RobotStuff.IndividualComponents.DemoBot.DemoBotLifter;
-import org.firstinspires.ftc.teamcode.RobotStuff.IndividualComponents.DemoBot.DemoBotPivot;
-import org.firstinspires.ftc.teamcode.RobotStuff.IndividualComponents.DriveModes.RobotCentricDrive;
-import org.firstinspires.ftc.teamcode.RobotStuff.Misc.DeltaTimer;
 import org.firstinspires.ftc.teamcode.RobotStuff.Config.Subconfigs.OpModeGroups;
+import org.firstinspires.ftc.teamcode.RobotStuff.IndividualComponents.DemoBot.DemoBotDrive;
+import org.firstinspires.ftc.teamcode.RobotStuff.IndividualComponents.DemoBot.DemoBotLifter;
+import org.firstinspires.ftc.teamcode.RobotStuff.Misc.DeltaTimer;
 
 import java.util.concurrent.TimeUnit;
 
-@TeleOp(name = "Demo Bot OpMode", group = OpModeGroups.TESTING)
+@TeleOp(name = "Lifter Test", group = OpModeGroups.TESTING)
 //@Disabled
-public class DemoBotOpMode extends NextFTCOpMode {
-    public DemoBotOpMode() {super(DemoBotPivot.INSTANCE, DemoBotLauncher.INSTANCE, DemoBotLifter.INSTANCE);}
+public class LifterTest extends NextFTCOpMode {
+    public LifterTest() {super(DemoBotLifter.INSTANCE);}
 
     DeltaTimer deltaTimer = new DeltaTimer();
     long deltaTimeNano;
@@ -30,17 +25,8 @@ public class DemoBotOpMode extends NextFTCOpMode {
         demoRobotConfig = new DemoRobotConfig(this);
         demoBotDrive = new DemoBotDrive(this, demoRobotConfig);
         DemoBotLifter.INSTANCE.initSystem(demoRobotConfig);
-        DemoBotPivot.INSTANCE.initSystem(demoRobotConfig);
-        DemoBotLauncher.INSTANCE.initSystem(demoRobotConfig);
     }
     @Override public void onStartButtonPressed() {
-        DemoBotLauncher.INSTANCE.map(demoRobotConfig.playerOne.RightTrigger, DemoBotLauncher.Mappings.SHOOT);
-        DemoBotLauncher.INSTANCE.map(demoRobotConfig.playerOne.RightBumper, DemoBotLauncher.Mappings.OUTTAKE);
-
-        DemoBotPivot.INSTANCE.map(demoRobotConfig.playerOne.DpadUp, DemoBotPivot.Mappings.UP);
-        DemoBotPivot.INSTANCE.map(demoRobotConfig.playerOne.DpadDown, DemoBotPivot.Mappings.DOWN);
-        DemoBotPivot.INSTANCE.map(demoRobotConfig.playerOne.DpadLeft, DemoBotPivot.Mappings.NORMAL);
-
         DemoBotLifter.INSTANCE.map(demoRobotConfig.playerOne.Triangle, DemoBotLifter.Mappings.COLLECT);
         DemoBotLifter.INSTANCE.map(demoRobotConfig.playerOne.Square, DemoBotLifter.Mappings.DEPOSIT);
         DemoBotLifter.INSTANCE.map(demoRobotConfig.playerOne.Circle, DemoBotLifter.Mappings.HOLD);
