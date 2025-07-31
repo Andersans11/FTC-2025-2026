8e1959554b5074297314eed41040947eee39b2f9
yahaha5788 <j.thomps1310@gmail.com>
2025-07-31 12:38:47 -0400

    lockyaw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStuff/IndividualComponents/DriveModes/HoldHeadingPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStuff/IndividualComponents/DriveModes/HoldHeadingPID.java
@@ -2,14 +2,16 @@ package org.firstinspires.ftc.teamcode.RobotStuff.IndividualComponents.DriveMode
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.IMU;
 import com.rowanmcalpin.nextftc.ftc.driving.MecanumDriverControlled;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.RobotStuff.Config.Subconfigs.RobotConfig;
+import org.firstinspires.ftc.teamcode.RobotStuff.Misc.DifferenceArrayList;
+import org.firstinspires.ftc.teamcode.RobotStuff.Misc.GoBildaPinpointDriver;
 import org.firstinspires.ftc.teamcode.RobotStuff.PIDStuff.YawPID;
 
 import kotlin.jvm.functions.Function0;
+import java.util.ArrayList;
 
 @Config
 public class HoldHeadingPID extends DriveMotors {
@@ -23,21 +25,23 @@ public class HoldHeadingPID extends DriveMotors {
     public static double secondarykD = 0.05;
     public static double threshold = Math.PI / 20;
 
-    double targetRad;
+    public static boolean DEBUGMODE = false;
+    public static boolean CLEARDIFF = false;
 
+    GoBildaPinpointDriver pinpoint;
     YawPID HeadingPID;
-    IMU imu;
 
-    boolean useNormTurn;
+    DifferenceArrayList differences = new DifferenceArrayList();
+
+    double targetRad;
     double targetHeading;
-
     long extDTN;
 
     Function0<Float> forwardBackward = () -> (float) (config.playerOne.ForwardAxis.getValue() * config.sensitivities.getForwardSensitivity() * getSensitivityMod());
     Function0<Float> strafe = () -> (float) (config.playerOne.StrafeAxis.getValue() * config.sensitivities.getStrafingSensitivity() * getSensitivityMod());
     Function0<Float> yaw = () -> {
         updateHeading(config.playerOne.TurnAxis.getValue() * config.sensitivities.getPIDturningSensitivity());
-        return (float) HeadingPID.lockYaw(targetRad, imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS), extDTN);
+        return (float) HeadingPID.lockYaw(targetRad,  pinpoint.getHeading(AngleUnit.RADIANS), extDTN);
     };
     /*
     for new coders:
@@ -61,22 +65,29 @@ public class HoldHeadingPID extends DriveMotors {
         super(opMode, config);
         HeadingPID = new YawPID(opMode.telemetry, config, "HeadingPID");
         HeadingPID.setSecondary(true);
-        imu = opMode.hardwareMap.get(IMU.class, "imu"); // ASSIGN IMU BEFORE RUNNING THIS CODE
-        setHeading(imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
+        pinpoint = opMode.hardwareMap.get(GoBildaPinpointDriver.class, "sensor");
+        setHeading(pinpoint.getHeading(AngleUnit.DEGREES)); // on init, take heading and set to that so that robot doesn't go to zero
         this.vroom = new MecanumDriverControlled(driveMotors, forwardBackward, strafe, yaw, true);
     }
 
     double getHeadingDeg() {
-        return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+        return pinpoint.getHeading(AngleUnit.DEGREES);
     }
 
-    public void telemetryAngleVelocity() {
-        if (useNormTurn) {opMode.telemetry.addLine("Mode: Using raw turn values");}
-        else {opMode.telemetry.addLine("Mode: Holding heading using IMU");}
+    public void runTelemetry() {
         opMode.telemetry.addLine("================================");
         opMode.telemetry.addData("current heading", getHeadingDeg());
         opMode.telemetry.addData("target heading", targetHeading);
-        opMode.telemetry.addData("target heading (radians)", targetRad);
+
+        if (DEBUGMODE) {
+            differences.add((float) Math.abs(targetHeading - getHeadingDeg()));
+
+            opMode.telemetry.addData("target heading (radians)", targetRad);
+            opMode.telemetry.addData("target - current difference", differences.lastAdded());
+            opMode.telemetry.addData("average difference", differences.getCollectiveAverage());
+            opMode.telemetry.addData("maximum difference", differences.getMax());
+            opMode.telemetry.addData("minimum difference", differences.getMin());
+        }
     }
 
     @Override
@@ -84,7 +95,12 @@ public class HoldHeadingPID extends DriveMotors {
 
         extDTN = deltaTimeNano; // for pid
 
-        telemetryAngleVelocity();
+        runTelemetry(); // runs telemetry logic
+
+        if (CLEARDIFF) {
+            differences.clear();
+            CLEARDIFF = false; // user sets to true, resets diff and goes to false
+        }
 
         HeadingPID.setCoefficients(kP, kI, kD);
         HeadingPID.setSecondaryCoefficients(secondarykP, secondarykI, secondarykD);
@@ -98,7 +114,7 @@ public class HoldHeadingPID extends DriveMotors {
         targetRad = Math.toRadians(targetHeading);
     }
 
-    public void updateHeading() {
+    public void updateHeading() { // if robot turns without joystick except no
         targetHeading = getHeadingDeg();
         targetRad = Math.toRadians(targetHeading);
     }
