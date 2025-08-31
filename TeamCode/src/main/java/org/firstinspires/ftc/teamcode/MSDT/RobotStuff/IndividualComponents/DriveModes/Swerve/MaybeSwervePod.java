e3714a7d6a3211f59c76dec425d49e4fbedc4b26
yahaha5788 <j.thomps1310@gmail.com>
2025-08-31 08:41:52 -0400

    mayhaps

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MSDT/RobotStuff/IndividualComponents/DriveModes/Swerve/MaybeSwervePod.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode.MSDT.RobotStuff.IndividualComponents.DriveModes.Swerve;
+
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.rowanmcalpin.nextftc.core.Subsystem;
+import com.rowanmcalpin.nextftc.ftc.hardware.controllables.MotorEx;
+import com.rowanmcalpin.nextftc.ftc.hardware.controllables.SetPower;
+
+public class MaybeSwervePod extends Subsystem {
+
+    MotorEx driveMotor;
+    SwervePivot pivot;
+
+    public double mag;
+    public double theta;
+
+    public SwerveVector oldVector;
+
+    public SwerveVector mirrorVector;
+
+    public static double kP = 0.1;
+    public static double kI = 0;
+    public static double kD = 0;
+
+    public MaybeSwervePod(MotorEx drive, CRServo pivot, AnalogInput encoder) {
+        driveMotor = drive;
+        this.pivot = new SwervePivot(pivot, encoder);
+        this.pivot.setPidCoeffs(kP, kI, kD);
+    }
+
+    public void init() {
+        pivot.changeTargetRotation(0);
+    }
+
+    public void initLoop() {
+        pivot.update();
+    }
+
+    public void update(SwerveVector driveVector) {
+
+        if (driveVector.getTheta() > Math.PI) {
+            mirrorVector = new SwerveVector(Math.max(-1, Math.min(driveVector.getMag(), 1)) * -1, driveVector.getTheta() + Math.PI);
+
+        } else {
+            mirrorVector = new SwerveVector(Math.max(-1, Math.min(driveVector.getMag(), 1)) * -1, driveVector.getTheta() - Math.PI);
+        }
+
+        if (oldVector.thetaDist(mirrorVector) < oldVector.thetaDist(driveVector)) {
+            mag = Math.max(-1, Math.min(mirrorVector.getMag(), 1));
+            theta = mirrorVector.getThetaDeg();
+            oldVector = mirrorVector;
+
+        } else {
+            mag = Math.max(-1, Math.min(driveVector.getMag(), 1));
+            theta = driveVector.getThetaDeg();
+            oldVector = driveVector;
+        }
+
+        pivot.changeTargetRotation(theta);
+        pivot.update();
+
+        new SetPower(driveMotor, mag).invoke();
+    }
+
+    public double getAmp() {
+        return mag;
+    }
+
+    public double getTheta() {
+        return theta;
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MSDT/RobotStuff/IndividualComponents/DriveModes/Swerve/SwerveVector.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.MSDT.RobotStuff.IndividualComponents.DriveModes.Swerve;
+
+import org.apache.commons.math3.exception.DimensionMismatchException;
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+
+public class SwerveVector {
+
+    private final double x; // x = mag cos(theta)
+    private final double y; // y = mag sin(theta)
+    private final double magnitude; // mag = sqrt(x^2 + y^2)
+    private final double theta; // theta = arctan(y / x)
+
+    public SwerveVector(double a, SwerveVector u) {
+        this.x = a * u.x;
+        this.y = a * u.y;
+
+        this.magnitude = Math.sqrt(x*x + y*y);
+        this.theta = Math.atan(y / x);
+    }
+
+    public SwerveVector(double scalar1, SwerveVector vec1, double scalar2, SwerveVector vec2, double scalar3, SwerveVector vec3) {
+        this.x = scalar1 * vec1.x + scalar2 * vec2.x + scalar3 * vec3.x;
+        this.y = scalar1 * vec1.y + scalar2 * vec2.y + scalar3 * vec3.y;
+
+        this.magnitude = Math.sqrt(x*x + y*y);
+        this.theta = Math.atan(y / x);
+    }
+
+    public SwerveVector(double magnitude, double theta) {
+        this.magnitude = magnitude;
+        this.theta = theta;
+
+        this.x = magnitude * Math.cos(theta);
+        this.y = magnitude * Math.sin(theta);
+    }
+
+    public double getTheta() {
+        return theta;
+    }
+
+    public double getThetaDeg() {
+        return Math.toDegrees(theta);
+    }
+
+    public double getMag() {
+        return magnitude;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double thetaDist(SwerveVector comp) {
+        return Math.abs(this.theta - comp.theta);
+    }
+
+}
